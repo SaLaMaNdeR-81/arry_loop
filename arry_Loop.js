105fecf440c051a897916baa5c595362ff9012b9
einollahi <einollahi.1366@gmail.com>
2022-04-20 08:17:15 +0430

    a better approach for your concern

--- arry_Loop.js
+++ arry_Loop.js
@@ -1,40 +1,31 @@
-const arry=[
-    "arry one",
-    "arry two",
-    "arry three",
-    "arry for",
-    "arry five",
-    "arry six",
-    "arry end"
-]
+const array = [
+  'arry one',
+  'arry two',
+  'arry three',
+  'arry four',
+  'arry five',
+  'arry six',
+  'arry end',
+];
 
+let limit = 0;
 
+// function arryloop() {
 
+//     console.log(arry[limit]);
 
+//     limit++;
 
+//     if(limit == arry.length){limit = 0}
 
+//     setTimeout(arryloop, 1000)
+//   }
 
+//   arryloop();
 
+let index = 0;
+setInterval(() => {
+  index = index >= array.length ? 0 : index;
 
-
-let limit = 0
-
-function arryloop() {
-
-
-    console.log(arry[limit]);
-
-    limit++;
-
-    if(limit == arry.length){limit = 0}
-
-
-    setTimeout(arryloop, 1000)
-  }
-  
-  arryloop();
-
-
-
-
-  
+  console.log(array[index++]);
+}, 1000);
